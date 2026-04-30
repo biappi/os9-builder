bc7d3257554288bc6c3b0549493961fbb150bafb
Enrico Gueli <enrico.gueli@gmail.com>
2026-04-30 18:24:34 +0200

    make ghidra-os9-copy-to-ram.py copy extra modules too

--- ghidra-os9-copy-to-ram.py
+++ ghidra-os9-copy-to-ram.py
@@ -15,8 +15,19 @@ def copy_memory_range(src_addr, dst_addr, length):
     except Exception as e:
         print "Error during copy: {}".format(e)
 
-
+# first stage (enough for RAM disk)
 start = 0xcd8
 end = 0xd080
 dest = 0x08010000
-copy_memory_range(toAddr(start), toAddr(dest), end - start)
+size = end - start
+copy_memory_range(toAddr(start), toAddr(dest), size)
+print "First stage copied from range 0x{:x}-0x{:x} to 0x{:x}-0x{:x}".format(start, end, dest, dest + size)
+
+# second stage (SCSI and floppy)
+# 0x2a6 bytes starting from 0xd0c0 (by looking at last nonzero RAM addr in MAME, 0x801c64d)
+stage2_start = 0xd0c0
+stage2_size = 0x2a6
+stage2_end = stage2_start + stage2_size
+stage2_dest = dest + size
+copy_memory_range(toAddr(stage2_start), toAddr(stage2_dest), stage2_size)
+print "Second stage copied from range 0x{:x}-0x{:x} to 0x{:x}-0x{:x}".format(stage2_start, stage2_end, stage2_dest, stage2_dest + stage2_size)
